34053330a4c6a80e8bc97f20de1bd018a7938354
andititan <pirinkovandi@gmail.com>
2023-01-09 17:52:31 +0200

    first commit

---
+++ build/CMakeFiles/cameraTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cameraTest.dir/main.cpp.o"
+  "CMakeFiles/cameraTest.dir/main.cpp.o.d"
+  "cameraTest"
+  "cameraTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cameraTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
